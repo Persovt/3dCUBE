894739deb3f7dbe799238600d82496909de5462d
Persovt <mza200339@gmail.com>
2021-03-29 11:54:17 +0300

    add 3d model

--- src/index.js
+++ src/index.js
@@ -2,53 +2,63 @@ import React, { createRef, useEffect } from "react";
 import ReactDOM from "react-dom";
 import * as THREE from "three";
 
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+
 const ref = createRef();
+
 const Main = () => {
-  
   useEffect(() => {
-    
     const scene = new THREE.Scene();
-  
+
     const camera = new THREE.PerspectiveCamera(
-      75,
+      45,
       window.innerWidth / window.innerHeight,
-      0.1,
+      1,
       1000
     );
-  
-    const renderer = new THREE.WebGLRenderer();
+
+    const renderer = new THREE.WebGLRenderer({ alpha: true });
+    renderer.setClearColor(0x000000, 0);
     renderer.setSize(window.innerWidth, window.innerHeight);
+
     ref.current.appendChild(renderer.domElement);
-    console.log(renderer.domElement);
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshToonMaterial();
+
     const light = new THREE.HemisphereLight(0xffffbb, 0x080820, 1);
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube, light);
-  
-    camera.position.z = 5;
-  
+    scene.add(light);
+
+    let loader = new GLTFLoader();
+    let obj = null;
+    loader.load("3d/scene.gltf", function (gltf) {
+      obj = gltf;
+      scene.add(obj.scene);
+    });
+
+    camera.position.z = 20;
+
+    let bool = false;
     const animate = function (event) {
-      console.log(event);
-  
-      cube.rotation.x = event.clientY * 0.01;
-      cube.rotation.y = -(event.clientX * 0.01);
-  
+      requestAnimationFrame(animate);
+
+      if (obj) {
+        obj.scene.rotation.y += 0.01;
+
+        if (camera.position.y > 19) bool = true;
+        if (camera.position.y < 0) bool = false;
+        if (bool) camera.position.y -= 0.05;
+        else camera.position.y += 0.05;
+      }
+
       renderer.render(scene, camera);
     };
-  
-    requestAnimationFrame(animate);
-    window.onmousemove = animate;
+
+    animate();
   }, []);
-  return (
-    <div className="" ref={ref}></div>
-  )
-}
+  return <div className="" ref={ref}></div>;
+};
 
 ReactDOM.render(
   <React.StrictMode>
-  <Main/>
-  
+    <Main />
   </React.StrictMode>,
   document.getElementById("root")
 );
